9f5222c418f6818c8197544b5326b7045fa16673
AO Industries, Inc <support@aoindustries.com>
2021-10-30 23:47:06 -0500

    Changed links to https for https://www.gnu.org/licenses/

--- book/src/main/java/module-info.java
+++ book/src/main/java/module-info.java
@@ -18,7 +18,7 @@
  * GNU Lesser General Public License for more details.
  *
  * You should have received a copy of the GNU Lesser General Public License
- * along with semanticcms-autogit-servlet.  If not, see <http://www.gnu.org/licenses/>.
+ * along with semanticcms-autogit-servlet.  If not, see <https://www.gnu.org/licenses/>.
  */
 module com.semanticcms.autogit.servlet.book {
 	exports com.semanticcms.autogit.servlet.book;
